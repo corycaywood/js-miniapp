33f58bc168fa34d6be1439e4e37d163cb79571d3
CI Publisher <dev-opensource@mail.rakuten.com>
2020-12-01 08:41:19 +0000

    export bridge resource [ci skip]

--- bridge.js
+++ bridge.js
@@ -1,17 +1,21 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
+/** @internal */
 Object.defineProperty(exports, "__esModule", { value: true });
-/* tslint:disable:no-any */
+var token_data_1 = require("./types/token-data");
+/** @internal */
 var mabMessageQueue = [];
 exports.mabMessageQueue = mabMessageQueue;
+/** @internal */
 var MiniAppBridge = /** @class */ (function () {
     function MiniAppBridge(executor) {
         this.executor = executor;
+        this.platform = executor.getPlatform();
     }
     /**
      * Success Callback method that will be called from native side
      * to this bridge. This method will send back the value to the
-     * mini apps that uses promises
+     * mini apps that uses promises.
      * @param  {[String]} messageId Message ID which will be used to get callback object from messageQueue
      * @param  {[String]} value Response value sent from the native on invoking the action command
      */
@@ -28,7 +32,7 @@ var MiniAppBridge = /** @class */ (function () {
     /**
      * Error Callback method that will be called from native side
      * to this bridge. This method will send back the error message to the
-     * mini apps that uses promises
+     * mini apps that uses promises.
      * @param  {[String]} messageId Message ID which will be used to get callback object from messageQueue
      * @param  {[String]} errorMessage Error message sent from the native on invoking the action command
      */
@@ -41,7 +45,7 @@ var MiniAppBridge = /** @class */ (function () {
         removeFromMessageQueue(queueObj);
     };
     /**
-     * Associating getUniqueId function to MiniAppBridge object
+     * Associating getUniqueId function to MiniAppBridge object.
      */
     MiniAppBridge.prototype.getUniqueId = function () {
         var _this = this;
@@ -50,13 +54,132 @@ var MiniAppBridge = /** @class */ (function () {
         });
     };
     /**
-     * Associating requestPermission function to MiniAppBridge object
-     * @param {String} permissionType Type of permission that is requested. For eg., location
+     * Associating requestPermission function to MiniAppBridge object.
+     * @param {DevicePermission} permissionType Type of permission that is requested. For eg., location
      */
     MiniAppBridge.prototype.requestPermission = function (permissionType) {
         var _this = this;
         return new Promise(function (resolve, reject) {
             return _this.executor.exec('requestPermission', { permission: permissionType }, function (success) { return resolve(success); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating showInterstitialAd function to MiniAppBridge object.
+     * @param {string} id ad unit id of the intertitial ad
+     */
+    MiniAppBridge.prototype.showInterstitialAd = function (id) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('showAd', { adType: 0 /* INTERSTITIAL */, adUnitId: id }, function (closeSuccess) { return resolve(closeSuccess); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating loadInterstitialAd function to MiniAppBridge object.
+     * This function preloads interstitial ad before they are requested for display.
+     * Can be called multiple times to pre-load multiple ads.
+     * @param {string} id ad unit id of the intertitial ad that needs to be loaded.
+     */
+    MiniAppBridge.prototype.loadInterstitialAd = function (id) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('loadAd', { adType: 0 /* INTERSTITIAL */, adUnitId: id }, function (loadSuccess) { return resolve(loadSuccess); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating loadRewardedAd function to MiniAppBridge object.
+     * This function preloads Rewarded ad before they are requested for display.
+     * Can be called multiple times to pre-load multiple ads.
+     * @param {string} id ad unit id of the Rewarded ad that needs to be loaded.
+     */
+    MiniAppBridge.prototype.loadRewardedAd = function (id) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('loadAd', { adType: 1 /* REWARDED */, adUnitId: id }, function (loadSuccess) { return resolve(loadSuccess); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating showRewardedAd function to MiniAppBridge object.
+     * @param {string} id ad unit id of the Rewarded ad
+     */
+    MiniAppBridge.prototype.showRewardedAd = function (id) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('showAd', { adType: 1 /* REWARDED */, adUnitId: id }, function (rewardResponse) { return resolve(JSON.parse(rewardResponse)); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating requestCustomPermissions function to MiniAppBridge object
+     * @param [CustomPermissionType[] permissionTypes, Types of custom permissions that are requested
+     * using an Array including the parameters eg. name, description.
+     *
+     * For eg., Miniapps can pass the array of valid custom permissions as following
+     * [
+     *  {"name":"rakuten.miniapp.user.USER_NAME", "description": "Reason to request for the custom permission"},
+     *  {"name":"rakuten.miniapp.user.PROFILE_PHOTO", "description": "Reason to request for the custom permission"},
+     *  {"name":"rakuten.miniapp.user.CONTACT_LIST", "description": "Reason to request for the custom permission"}
+     * ]
+     */
+    MiniAppBridge.prototype.requestCustomPermissions = function (permissionTypes) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('requestCustomPermissions', { permissions: permissionTypes }, function (success) { return resolve(JSON.parse(success)); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating shareInfo function to MiniAppBridge object.
+     * This function returns the shared info action state.
+     * @param {info} The shared info object.
+     */
+    MiniAppBridge.prototype.shareInfo = function (info) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('shareInfo', { shareInfo: info }, function (success) { return resolve(success); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating getUserName function to MiniAppBridge object.
+     * This function returns username from the user profile
+     * (provided the rakuten.miniapp.user.USER_NAME custom permission is allowed by the user)
+     * It returns error info if user had denied the custom permission
+     */
+    MiniAppBridge.prototype.getUserName = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('getUserName', null, function (userName) { return resolve(userName); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Associating getProfilePhoto function to MiniAppBridge object.
+     * This function returns username from the user profile
+     * (provided the rakuten.miniapp.user.PROFILE_PHOTO is allowed by the user)
+     * It returns error info if user had denied the custom permission
+     */
+    MiniAppBridge.prototype.getProfilePhoto = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('getProfilePhoto', null, function (profilePhoto) { return resolve(profilePhoto); }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * Get access token from native hostapp.
+     */
+    MiniAppBridge.prototype.getAccessToken = function () {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('getAccessToken', null, function (tokenData) {
+                var nativeTokenData = JSON.parse(tokenData);
+                resolve(new token_data_1.AccessTokenData(nativeTokenData));
+            }, function (error) { return reject(error); });
+        });
+    };
+    /**
+     * This function does not return anything back on success.
+     * @param {screenAction} The screen state that miniapp wants to set on device.
+     */
+    MiniAppBridge.prototype.setScreenOrientation = function (screenAction) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            return _this.executor.exec('setScreenOrientation', { action: screenAction }, function (success) { return resolve(success); }, function (error) { return reject(error); });
         });
     };
     return MiniAppBridge;
@@ -66,6 +189,8 @@ exports.MiniAppBridge = MiniAppBridge;
  * Method to remove the callback object from the message queue after successfull/error communication
  * with the native application
  * @param  {[Object]} queueObj Queue Object that holds the references of callback informations
+ *
+ * @internal
  */
 function removeFromMessageQueue(queueObj) {
     var messageObjIndex = mabMessageQueue.indexOf(queueObj);
@@ -74,10 +199,11 @@ function removeFromMessageQueue(queueObj) {
     }
 }
 
-},{}],2:[function(require,module,exports){
+},{"./types/token-data":4}],2:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var common_bridge_1 = require("../common-bridge");
+var platform_1 = require("../types/platform");
 /* tslint:disable:no-any */
 var uniqueId = Math.random();
 // tslint:disable-next-line: variable-name
@@ -96,6 +222,9 @@ var IOSExcecutor = /** @class */ (function () {
         callback.id = String(++uniqueId);
         common_bridge_1.mabMessageQueue.unshift(callback);
         window.webkit.messageHandlers.MiniAppiOS.postMessage(JSON.stringify({ action: action, param: param, id: callback.id }));
+    };
+    IOSExcecutor.prototype.getPlatform = function () {
+        return platform_1.Platform.IOS;
     };
     return IOSExcecutor;
 }());
@@ -116,4 +245,28 @@ navigator.geolocation.getCurrentPosition = function (success, error, options) {
     }, function (error) { return console.error(error); });
 };
 
-},{"../common-bridge":1}]},{},[2]);
+},{"../common-bridge":1,"../types/platform":3}],3:[function(require,module,exports){
+"use strict";
+/** @internal */
+Object.defineProperty(exports, "__esModule", { value: true });
+/** Device platform. */
+var Platform;
+(function (Platform) {
+    Platform["ANDROID"] = "Android";
+    Platform["IOS"] = "iOS";
+})(Platform = exports.Platform || (exports.Platform = {}));
+
+},{}],4:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/** Token data type. */
+var AccessTokenData = /** @class */ (function () {
+    function AccessTokenData(baseToken) {
+        this.token = baseToken.token;
+        this.validUntil = new Date(baseToken.validUntil);
+    }
+    return AccessTokenData;
+}());
+exports.AccessTokenData = AccessTokenData;
+
+},{}]},{},[2]);
